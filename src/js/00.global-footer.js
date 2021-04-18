1dd5c2205911cbb9d4a713050d322cbcc2cc8c43
jcackowski <imjuon@gmail.com>
2021-04-18 18:19:35 -0400

    dist optimization

--- src/js/00.global-footer.js
+++ src/js/00.global-footer.js
@@ -1,8 +1,6 @@
 import $ from 'jquery';
 import 'jquery-scrollify';
-// import 'slick-carousel';
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel';
 import '../scss/00.global.scss';
 
 window.jQuery = $;
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,38 @@
-const path = require('path');
+const path = require('path')
+const glob = require('glob')
 const extract = require("mini-css-extract-plugin");
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const TerserPlugin = require('terser-webpack-plugin');
+const entries = glob.sync('./src/js/*').reduce((entries, entry) => {
+    const entryName = path.parse(entry).name
+    entries[entryName] = entry
+
+    return entries
+}, {});
 
 module.exports = {
-    entry: {
-        global: {
-            import: './src/js/00.global.js',
-            // dependOn: 'shared',
-        },
-        who: {
-            import: './src/js/02.who.js',
-            // dependOn: 'shared',
-        },
-        shared: 'jquery',
-    },
+    // entry: {
+    //     globalfooter: {
+    //         import: './src/js/00.global-footer.js',
+    //         // dependOn: 'shared',
+    //     },
+    //     globalhead: {
+    //         import: './src/js/00.global-head.js',
+    //         // dependOn: 'shared',
+    //     },
+    //     who: {
+    //         import: './src/js/02.who.js',
+    //         // dependOn: 'shared',
+    //     },
+    //     who: {
+    //         import: './src/js/02.who.js',
+    //         // dependOn: 'shared',
+    //     }
+    // },
+    entry: entries,
     output: {
-        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js',
     },
     module: {
         rules: [
@@ -58,29 +74,32 @@ module.exports = {
                     }
                 ]
             },
-            // {
-            //     test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-            //     use: [
-            //         {
-            //             loader: 'url-loader',
-            //             options: {
-            //                 limit: 100000,
-            //                 name: '[name].[ext]'
-            //             }
-            //         }
-            //     ]
-            // },
-
+            {
+                test: /\.(eot|woff|woff2|ttf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'fonts'
+                        }
+                    }
+                ]
+            }
         ]
     },
     plugins: [
         new extract({
-            filename: '[name].css'
+            filename: 'css/[name].css'
         }),
         // new BundleAnalyzerPlugin()
     ],
     externals: {
         jquery: 'jQuery'
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({
+            extractComments: false,
+        })],
     },
     mode: 'production'
 }
