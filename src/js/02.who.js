5fc624219f792369a673d9e4daecd4c6119cf627
jcackowski <imjuon@gmail.com>
2021-04-18 16:36:16 -0400

    test who bundle cleanup

--- src/js/02.who.js
+++ src/js/02.who.js
@@ -5,38 +5,45 @@ import '../scss/02.who.scss';
 
 window.jQuery = $;
 
-$(document).ready(function () {
-    $('.tl-point').tooltipster({
-        maxWidth: 400,
-        theme: ['tooltipster-noir', 'tooltipster-timeline'],
-        trigger: 'custom',
-        triggerOpen: {
-            mouseenter: true,
-            // click: true,
-            tap: true    // For touch device
-        },
-        triggerClose: {
-            mouseleave: true,
-            // click: true,
-            tap: true    // For touch device
-        }
-    })
-    $('.loc').tooltipster({
-        theme: ['tooltipster-noir', 'tooltipster-locations']
-    })
-    // $('#heroslide5').click(function (e) {
-    //     e.preventDefault();
-    //     $('.hero-link').removeClass('active');
-    //     $(this).addClass('active');
-    //     $('.w-round div:nth-child(5)').trigger('tap');
-    // });
-    $('.next-link').click(function (e) {
-        e.preventDefault();
-        $('.w-slider-arrow-right').trigger('tap')
-    });
-    $('.prev-link').click(function (e) {
-        e.preventDefault();
-        $('.w-slider-arrow-left').trigger('tap')
-    });
 
+$('.tl-point').tooltipster({
+    maxWidth: 400,
+    theme: ['tooltipster-noir', 'tooltipster-timeline'],
+    trigger: 'custom',
+    triggerOpen: {
+        mouseenter: true,
+        // click: true,
+        tap: true    // For touch device
+    },
+    triggerClose: {
+        mouseleave: true,
+        // click: true,
+        tap: true    // For touch device
+    }
+})
+$('.loc').tooltipster({
+    theme: ['tooltipster-noir', 'tooltipster-locations']
+})
+// $('#heroslide5').click(function (e) {
+//     e.preventDefault();
+//     $('.hero-link').removeClass('active');
+//     $(this).addClass('active');
+//     $('.w-round div:nth-child(5)').trigger('tap');
+// });
+$('.next-link').click(function (e) {
+    e.preventDefault();
+    $('.w-slider-arrow-right').trigger('tap')
+});
+$('.prev-link').click(function (e) {
+    e.preventDefault();
+    $('.w-slider-arrow-left').trigger('tap')
+});
+
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+    adaptiveHeight: true
 });
