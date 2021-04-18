c2e4da85a1fe6672e618efc45a61b238b1979216
jcackowski <imjuon@gmail.com>
2021-04-18 17:07:45 -0400

    slick slider fix 1

--- webpack.config.js
+++ webpack.config.js
@@ -57,14 +57,27 @@ module.exports = {
                         }
                     }
                 ]
-            }
+            },
+            {
+                test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 100000,
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            },
+
         ]
     },
     plugins: [
         new extract({
             filename: '[name].css'
         }),
-        new BundleAnalyzerPlugin()
+        // new BundleAnalyzerPlugin()
     ],
     externals: {
         jquery: 'jQuery'
