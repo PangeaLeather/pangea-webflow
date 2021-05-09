a2fe51f976e85742feb2e7c0f4c22ffb1c71cf29
jcackowski <imjuon@gmail.com>
2021-05-09 13:27:19 -0400

    javascript error troubleshooting

--- src/js/00.global-footer.js
+++ src/js/00.global-footer.js
@@ -84,18 +84,18 @@ $(".menu-toggle").click(function () {
     $("body").toggleClass("body-freeze");
 });
 
-$(".menu-small-link").each(function () {
-    var data_page = $(this).data("page");
-    var href = $(this).attr("href");
-    if (window.location.href.indexOf(data_page) > -1) {
-        $(this).click(function () {
-            $.scrollify.instantMove(href);
-        });
-    }
-    else {
-        $(this).attr("href", "/" + data_page + href);
-    }
-});
+// $(".menu-small-link").each(function () {
+//     var data_page = $(this).data("page");
+//     var href = $(this).attr("href");
+//     if (window.location.href.indexOf(data_page) > -1) {
+//         $(this).click(function () {
+//             $.scrollify.instantMove(href);
+//         });
+//     }
+//     else {
+//         $(this).attr("href", "/" + data_page + href);
+//     }
+// });
 
 $('.panel-slickslider').each(function () {
     $(this).slick({
